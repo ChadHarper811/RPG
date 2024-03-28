11bdb6e7fbe5a73bcf32859e91fb13e14b88e84e
Chad Harper <chadharper811@gmail.com>
2024-03-27 21:37:29 -0500

    step 85

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+    {
+        name: "stick",
+        power: 5
+    },
+    {
+        name: "dagger",
+        power: 30
+    },
+    {
+        name: "claw hammer",
+        power: 50
+    },
+    {
+        name: "sword",
+        power: 100
+    }
+];
 const locations = [
     {
       name: "town square",
@@ -69,14 +87,23 @@ function fightDragon() {
 }
 
 function buyHealth() {
-    gold -= 10;
+    if (gold >= 10) {
+        gold -= 10;
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
+    }
 }
 
 function buyWeapon(){
-
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        text.innerText = "You now have a new weapon.";
+    }
 }
 
 function fightSlime() {
