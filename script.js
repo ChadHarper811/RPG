7e03e014ddc3fe3aefee5a26ba1a8d069c0c2b23
Chad Harper <chadharper811@gmail.com>
2024-03-27 21:27:41 -0500

    step 75

--- script.js
+++ script.js
@@ -28,6 +28,12 @@ const locations = [
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
   ];
 
@@ -47,15 +53,15 @@ function update(location){
 }
 
 function goTown() {
-    update(locations[0])
+    update(locations[0]);
 }
 
 function goStore(){
-
+    update(locations[1]);
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {
@@ -63,10 +69,21 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
 }
 
 function buyWeapon(){
+
+}
+
+function fightSlime() {
+
+}
+
+function fightBeast() {
 
 }
 
