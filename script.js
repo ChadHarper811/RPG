d69f8c21fa01f463a0edb7cdc8acbd33b8313443
Chad Harper <chadharper811@gmail.com>
2024-03-27 21:48:08 -0500

    step 95

--- script.js
+++ script.js
@@ -98,11 +98,18 @@ function buyHealth() {
 }
 
 function buyWeapon(){
-    if (gold >= 30) {
-        gold -= 30;
-        currentWeapon++;
-        goldText.innerText = gold;
-        text.innerText = "You now have a new weapon.";
+    if (currentWeapon < weapons.length) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory;
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
     }
 }
 
