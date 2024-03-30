d22a097318c755c7137311ea4290befe14ffbcec
Chad Harper <chadharper811@gmail.com>
2024-03-30 17:02:11 -0500

    step 152

--- script.js
+++ script.js
@@ -181,8 +181,12 @@ function goFight() {
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-    health -= monsters[fighting].level;
-    monsterHealth -= weapons[currentWeapon].power + (Math.floor(Math.random() * xp) + 1);
+    health -= getMonsterAttackValue(monsters[fighting].level);
+    if (isMonsterHit()) {
+        monsterHealth -= weapons[currentWeapon].power + (Math.floor(Math.random() * xp) + 1);
+    } else {
+        text.innerText += " You miss."
+    };
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
     if (health <= 0) {
@@ -193,7 +197,20 @@ function attack() {
         } else {
             defeatMonster();
         }
-    }
+    };
+    if (Math.random() <= .1) {
+
+    };
+}
+
+function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
 }
 
 function dodge() {
