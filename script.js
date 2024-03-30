726e9f971c05b4c6b915bc1d815b2aedcbb23565
Chad Harper <chadharper811@gmail.com>
2024-03-30 18:41:49 -0500

    step 171

--- script.js
+++ script.js
@@ -259,6 +259,18 @@ function easterEgg() {
 
 function pick(guess) {
     const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    };
+    text.innerText = "You picked " + guess +". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n";
+    };
+    if (numbers.includes(guess)) {
+        text.innerText += "Right! You win 20 gold!";
+        gold += 20;
+        goldText.innerText = gold;
+    }
 }
 
 function pickTwo() {
