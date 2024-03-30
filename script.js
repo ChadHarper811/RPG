4a47f6573a083e1ff7ba3b34239f567cfce3ad96
Chad Harper <chadharper811@gmail.com>
2024-03-30 16:33:44 -0500

    step 140

--- script.js
+++ script.js
@@ -163,9 +163,28 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= monsters[fighting].level;
-    monsterHealth -= weapons[currentWeapon].power;
+    monsterHealth -= weapons[currentWeapon].power + (Math.floor(Math.random() * xp) + 1);
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+       lose();
+    } else if (monsterHealth <= 0) {
+        defeatMonster();
+    }
 }
 
 function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+}
+
+function lose() {
 
 }
